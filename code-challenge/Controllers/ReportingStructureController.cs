70457725978f807c0f0a980dd1045c58b362ae3a
PUS\nbartolotti <nbartolotti@pharos.com>
2021-05-02 21:30:09 -0400

    Initial commit

---
+++ code-challenge/Controllers/ReportingStructureController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using challenge.Services;
+using challenge.Models;
+
+namespace challenge.Controllers
+{
+    [Route("api/reporting")]
+    public class ReportingStructureController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly IEmployeeService _employeeService;
+
+        public ReportingStructureController(ILogger<EmployeeController> logger, IEmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("{id}", Name = "getDirectReports")]
+        public IActionResult GetDirectReports(String id)
+        {
+            _logger.LogDebug($"Received direct report get request for '{id}'");
+
+            var employee = _employeeService.GetById(id);
+
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+    }
+}
---
+++ code-challenge/Models/ReportingStructure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace challenge.Models
+{
+    public class ReportingStructure
+    {
+        public String Employee { get; set; }
+        public int NumberOfReports { get; set; }
+
+    }
+}
